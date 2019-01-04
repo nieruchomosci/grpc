956a514dd4da37cfff2907dac1661ebc93beab78
Michal Kruczek <partikus@gmail.com>
2019-01-04 16:56:29 +0100

    gRPC - switch to fixed version of grpc-go 1.17.0 and protobuf 1.2.0

--- protoc.sh
+++ protoc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-PROTOBUF_REVISION=${PROTOBUF_REVISION:-"3.6.1"}
+PROTOBUF_REVISION=${PROTOBUF_REVISION:-"3.6.2"}
 GLIBC_VERSION=${GLIBC_VERSION:-"2.23-r3"}
 ALPINE_GLIBC_URL=${ALPINE_GLIBC_URL:-"https://github.com/sgerrand/alpine-pkg-glibc/releases/download/${GLIBC_VERSION}/"}
 GLIBC_PKG=${GLIBC_PKG:-"glibc-${GLIBC_VERSION}.apk"}
@@ -8,7 +8,7 @@ GLIBC_BIN_PKG=${GLIBC_BIN_PKG:-"glibc-bin-${GLIBC_VERSION}.apk"}
 
 apk add --no-cache curl \
     && curl -sLO https://github.com/google/protobuf/releases/download/v${PROTOBUF_REVISION}/protoc-${PROTOBUF_REVISION}-linux-x86_64.zip \
-    && unzip protoc-${PROTOBUF_REVISION}-linux-x86_64.zip -d ./usr/local \
+    && unzip protoc-${PROTOBUF_REVISION}-linux-x86_64.zip -d /usr/local \
     && chmod +x /usr/local/bin/protoc \
     && chmod -R 755 /usr/local/include/ \
     && rm protoc-${PROTOBUF_REVISION}-linux-x86_64.zip
